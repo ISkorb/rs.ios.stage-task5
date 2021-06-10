4f4f7dbbfa4ec2f420943936601b1d2b457a8ada
ISkorb <ina.skorb@gmail.com>
2021-06-11 00:22:07 +0300

    feat: add implementation of exersise 1

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,26 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var profit:Int = 0
+        var maximum:Int
+        let count = prices.count
+        if(count > 0){
+            for _ in 0...count-1{
+                maximum = prices[count-1]
+                profit = 0
+                
+                if(count > 1){
+                    for k in (0...count-2).reversed() {
+                        maximum = max(maximum, prices[k+1])
+                        
+                        if(prices[k] < maximum){
+                            profit+=(maximum - prices[k])
+                        }
+                    }
+                }
+            }
+        }
+        
+        return profit
     }
 }
